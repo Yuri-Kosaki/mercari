c580d6ad6d3c92b2f0e1c50d84be7b54d374acdc
yuri <bebejuli7339@gmail.com>
2019-05-02 04:00:32 +0900

    デプロイの設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,7 +42,7 @@
 lock '3.11.0'
 
 set :application, 'mercari'
-set :repo_url,  'git@github.com:THitokuse/mercari.git'
+set :repo_url,  'git@github.com:Yuri-Kosaki/mercari.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
@@ -50,7 +50,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/Users/nezu/.ssh/mercari2.pem']
+                  keys: ['/Users/yuri/.ssh/mercari.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '52.194.135.137', user: 'ec2-user', roles: %w{app db web}
+server '52.192.45.141', user: 'ec2-user', roles: %w{app db web}
