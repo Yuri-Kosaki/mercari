ef5d462ed5eeeec639d7c743ced1bdb88b3f0c34
yuri <bebejuli7339@gmail.com>
2019-06-05 17:06:25 +0900

    s3の設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -15,8 +15,8 @@ CarrierWave.configure do |config|
     aws_secret_access_key: Rails.application.credentials.aws_secret_access_key,
       region: 'ap-northeast-1'
     }
-    config.fog_directory  = 'mentor-mercari'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/mentor-mercari'
+    config.fog_directory  = 'mercari-for-yuri'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/mercari-for-yuri'
   end
 
 end
